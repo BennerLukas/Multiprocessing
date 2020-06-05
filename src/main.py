7d6de9fb8e927fda6af9923ed920e8c718ab06e9
Lukas <lukas_benner@web.de>
2020-06-05 13:13:43 +0200

    basic structure

--- src/main.py
+++ src/main.py
@@ -1,3 +1,46 @@
+import numpy as np 
+import time
 from multiprocessing import Pool
 
-print("hi")
+
+
+def create_points(n=10):
+    'creates 3D Points'
+    pass
+    return points
+
+def calc_distance(x1, x2):
+    'calculate distance between 2 Points'
+    pass
+
+def get_distances(x1, points):
+    'calculate distance to every other point'
+    pass
+
+
+
+
+#---------------main-------------------#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def f(x):
+#     return x**2
+
+# if __name__ == '__main__':
+#     with  Pool(1) as p:
+#         print(p.map(f,[1,2,3,4,6,5]))
+
+# t0=time.time()
+# print(f'Zeit: {(time.time()-t0):.2f}s')
