4baa4e124772620dbbd767a00e41efe246ba78f2
Lukas <lukas_benner@web.de>
2020-06-05 13:21:10 +0200

    calc distance

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import time
+import math
 from multiprocessing import Pool
 
 
@@ -11,7 +12,9 @@ def create_points(n=10):
 
 def calc_distance(x1, x2):
     'calculate distance between 2 Points'
-    pass
+    distance = np.linalg.norm(x1-x2)
+    
+    return distance
 
 def get_distances(x1, points):
     'calculate distance to every other point'
@@ -21,9 +24,16 @@ def get_distances(x1, points):
 
 
 #---------------main-------------------#
+t0=time.time()
+
+x1= np.array([1,1])
+x2=np.array([1,3])
 
 
+print(calc_distance(x1,x2))
 
+
+print(f'Zeit: {(time.time()-t0):.5f}s')
 
 
 
